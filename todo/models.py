6719f3c5446ec83bf97463eed18494b803f82879
unknown <danilabrehov@gmail.com>
2020-10-31 18:45:55 +0300

    api work version with admin panel

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+  title = models.CharField(max_length=150)
+  description = models.CharField(max_length=300, blank=True)
+  date = models.DateTimeField(auto_now_add=True)
+  done = models.BooleanField(default=False)
+
+  def _str_(self):
+    return self.title
